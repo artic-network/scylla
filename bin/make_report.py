9da6c1aab7006ad9e4c4101569d1830e584b88d4
Biowilko <sam1.wilkinson1@gmail.com>
2025-04-17 16:11:33 +0100

    Make warning jsons fully optional

--- bin/make_report.py
+++ bin/make_report.py
@@ -50,22 +50,30 @@ def make_output_report(
         print("Generating: " + f"{report_to_generate}")
         fw.write(buf.getvalue())
 
+
 def get_binned_data(list_vals, num_bins, start=0):
     max_val = max(list_vals) + 1
-    print (max_val)
+    print(max_val)
     step = int((max_val - start) / num_bins) + ((max_val - start) % num_bins > 0)
     if step > 100:
-        step = (int(step/100) + 1) *100
+        step = (int(step / 100) + 1) * 100
     print(step)
     binned_data = []
     for i in range(num_bins):
-        binned_data.append({'bin_start':start + step*i, 'bin_end': start + step*(i+1), 'count':0})
+        binned_data.append(
+            {
+                "bin_start": start + step * i,
+                "bin_end": start + step * (i + 1),
+                "count": 0,
+            }
+        )
     for j in list_vals:
         for bin in binned_data:
-            if bin['bin_start'] <= j < bin['bin_end']:
-                bin['count'] += 1
+            if bin["bin_start"] <= j < bin["bin_end"]:
+                bin["count"] += 1
     print(binned_data)
     return binned_data, step
+
 
 def summarize_read_counts(read_counts_file, num_bins=42, start=0):
     with open(read_counts_file.resolve(), "rt") as qc_file:
@@ -75,9 +83,9 @@ def summarize_read_counts(read_counts_file, num_bins=42, start=0):
         for row in reader:
             lens.append(int(row["read_length"]))
             quals.append(float(row["mean_quality"]))
-    len_data, len_step =  get_binned_data(lens, num_bins, start)
+    len_data, len_step = get_binned_data(lens, num_bins, start)
     qual_data, qual_step = get_binned_data(quals, num_bins, start)
-    return  len_data, len_step, qual_data 
+    return len_data, len_step, qual_data
 
 
 def main():
@@ -94,7 +102,11 @@ def main():
         "--read_counts", help="JSON file of read_counts", required=False, type=Path
     )
     parser.add_argument(
-        "--warnings", help="text file containing any warnings", required=False, nargs='*', type=Path
+        "--warnings",
+        help="text file containing any warnings",
+        required=False,
+        nargs="*",
+        type=Path,
     )
     parser.add_argument(
         "--sample_id", help="Unique ID of sample", required=False, default="sample"
@@ -106,7 +118,11 @@ def main():
         "--template", help="HTML template for report", default="scylla.mako.html"
     )
     parser.add_argument("--classifier", help="Classifier used", default="Kraken")
-    parser.add_argument("--classification_database", help="Database used for classification", default="PlusPF")
+    parser.add_argument(
+        "--classification_database",
+        help="Database used for classification",
+        default="PlusPF",
+    )
     parser.add_argument("--version", help="Scylla version", default="unknown")
 
     args = parser.parse_args()
@@ -123,19 +139,29 @@ def main():
                 assignments = assignments[:-1] + ", " + contents[1:]
 
     if args.read_counts:
-        read_length_counts, read_length_step, read_quality_counts = summarize_read_counts(args.read_counts)
+        read_length_counts, read_length_step, read_quality_counts = (
+            summarize_read_counts(args.read_counts)
+        )
     else:
         read_length_counts, read_length_step, read_quality_counts = [], 2, []
 
     warnings = ""
-    if len(args.warnings) > 0:
+    if args.warnings:
         for warning_file in args.warnings:
             if os.path.getsize(warning_file) > 0:
                 with open(warning_file, "r") as f:
                     content = json.load(f)
                     warnings += content["msg"]
 
-    data_for_report = {"sankey_data": assignments, "read_length_data": read_length_counts, "read_length_step": read_length_step, "read_quality_data": read_quality_counts, "classifier": args.classifier, "classification_database": args.classification_database, "warnings": warnings}
+    data_for_report = {
+        "sankey_data": assignments,
+        "read_length_data": read_length_counts,
+        "read_length_step": read_length_step,
+        "read_quality_data": read_quality_counts,
+        "classifier": args.classifier,
+        "classification_database": args.classification_database,
+        "warnings": warnings,
+    }
 
     outfile = args.prefix + "_report.html"
     make_output_report(outfile, args.template, args.version, sample, data_for_report)
--- modules/check_hcid_status.nf
+++ modules/check_hcid_status.nf
@@ -7,28 +7,28 @@ process check_hcid {
     conda "bioconda::mappy=2.28 bioconda::pyfastx=2.1.0"
     container "community.wave.seqera.io/library/mappy_pyfastx:b4cc4b80f5e5decf"
 
-    publishDir "${params.outdir}/${unique_id}/qc/", mode: 'copy', pattern: '*.warning.json', optional:true
-    publishDir "${params.outdir}/${unique_id}/qc/", mode: 'copy', pattern: '*.reads.fq', optional:true
-    publishDir "${params.outdir}/${unique_id}/qc/", mode: 'copy', pattern: 'hcid.counts.csv'
-
+    publishDir "${params.outdir}/${unique_id}/qc/", mode: 'copy'
 
     input:
-        tuple val(unique_id), val(database_name), path(kreport), path(reads)
-        path taxonomy
-        path hcid_defs
-        path hcid_refs
+    tuple val(unique_id), val(database_name), path(kreport), path(reads)
+    path taxonomy
+    path hcid_defs
+    path hcid_refs
+
     output:
-        tuple val(unique_id), path("*warning.json"), emit: warnings
-        tuple val(unique_id), path("*reads.fq"), emit: reads
-        tuple val(unique_id), path("hcid.counts.csv"), emit: counts
+    tuple val(unique_id), path("*warning.json"), emit: warnings, optional: true
+    tuple val(unique_id), path("*reads.fq"), emit: reads, optional: true
+    tuple val(unique_id), path("hcid.counts.csv"), emit: counts
+
     script:
-        preset = ""
-        if ( params.read_type == "illumina") {
-            preset = "--illumina"
-        } else if ( params.paired ) {
-            preset = "--illumina"
-        }
-        """
+    preset = ""
+    if (params.read_type == "illumina") {
+        preset = "--illumina"
+    }
+    else if (params.paired) {
+        preset = "--illumina"
+    }
+    """
         check_hcid.py \
             -k ${kreport} \
             -r ${reads} \
@@ -36,28 +36,23 @@ process check_hcid {
             -i ${hcid_defs} \
             -d ${hcid_refs} \
             -p "hcid" ${preset}
-
-        PATTERN=(*.warning.json)
-        if [ ! -f \${PATTERN[0]} ]; then
-            echo "Found no warning files"
-            touch no_warning.json
-            touch no_reads.fq
-        fi
         """
 }
 
 workflow check_hcid_status {
     take:
-        kreport_ch
-        fastq_ch
-        taxonomy
-    main:
-        hcid_defs = file("$projectDir/resources/hcid.json")
-        hcid_refs = file("$projectDir/resources/hcid_refs.fa.gz")
+    kreport_ch
+    fastq_ch
+    taxonomy
 
-        kreport_ch.join(fastq_ch).set{input_ch}
-        check_hcid(input_ch, taxonomy, hcid_defs, hcid_refs)
-        check_hcid.out.warnings.set{ warning_ch }
+    main:
+    hcid_defs = file("${projectDir}/resources/hcid.json")
+    hcid_refs = file("${projectDir}/resources/hcid_refs.fa.gz")
+
+    kreport_ch.join(fastq_ch).set { input_ch }
+    check_hcid(input_ch, taxonomy, hcid_defs, hcid_refs)
+    check_hcid.out.warnings.set { warning_ch }
+
     emit:
-        warning_ch
+    warning_ch
 }
